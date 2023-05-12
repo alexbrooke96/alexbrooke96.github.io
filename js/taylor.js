fd4e2349e4706ba180694f452d6a1e6f4194629d
Alex Roy <alexroy@Alexs-MacBook-Pro.local>
2023-05-11 17:53:32 -0700

    Init

---
+++ js/taylor.js
@@ -0,0 +1,312 @@
+var songs = [['Tim McGraw','Taylor Swift'],
+['Picture to Burn','Taylor Swift'],
+['Teardrops on My Guitar','Taylor Swift'],
+['A Place in this World','Taylor Swift'],
+['Cold As You','Taylor Swift'],
+['The Outside','Taylor Swift'],
+['Tied Together with a Smile','Taylor Swift'],
+['Stay Beautiful','Taylor Swift'],
+['Should\'ve Said No','Taylor Swift'],
+['Mary\'s Song (Oh My My My)','Taylor Swift'],
+['Our Song','Taylor Swift'],
+['I\'m Only Me When I\'m With You','Taylor Swift'],
+['Invisible','Taylor Swift'],
+['A Perfectly Good Heart','Taylor Swift'],
+['Jump Then Fall','Fearless'],
+['Untouchable','Fearless'],
+['Come In With The Rain','Fearless'],
+['SuperStar','Fearless'],
+['The Other Side Of the Door','Fearless'],
+['Fearless','Fearless'],
+['Fifteen','Fearless'],
+['Love Story','Fearless'],
+['Hey Stephen','Fearless'],
+['White Horse','Fearless'],
+['You Belong With Me','Fearless'],
+['Breathe','Fearless'],
+['Tell Me Why','Fearless'],
+['You\'re Not Sorry','Fearless'],
+['The Way I Loved You','Fearless'],
+['Forever & Always','Fearless'],
+['The Best Day','Fearless'],
+['Change','Fearless'],
+['Mine','Speak Now'],
+['Sparks Fly','Speak Now'],
+['Back To December','Speak Now'],
+['Speak Now','Speak Now'],
+['Dear John','Speak Now'],
+['Mean','Speak Now'],
+['The Story Of Us','Speak Now'],
+['Never Grow Up','Speak Now'],
+['Enchanted','Speak Now'],
+['Better Than Revenge','Speak Now'],
+['Innocent','Speak Now'],
+['Haunted','Speak Now'],
+['Last Kiss','Speak Now'],
+['Long Live','Speak Now'],
+['Ours','Speak Now'],
+['If This Was A Movie','Speak Now'],
+['Superman','Speak Now'],
+['State of Grace','Red'],
+['Red','Red'],
+['Treacherous','Red'],
+['I Knew You Were Trouble.','Red'],
+['All Too Well','Red'],
+['22','Red'],
+['I Almost Do','Red'],
+['We Are Never Ever Getting Back Together','Red'],
+['Stay Stay Stay','Red'],
+['The Last Time','Red'],
+['Holy Ground','Red'],
+['Sad Beautiful Tragic','Red'],
+['The Lucky One','Red'],
+['Everything Has Changed','Red'],
+['Starlight','Red'],
+['Begin Again','Red'],
+['The Moment I knew','Red'],
+['Come Back...Be Here','Red'],
+['Girls At Home','Red'],
+['Welcome to New York','1989'],
+['Blank Space','1989'],
+['Style','1989'],
+['Out of the Woods','1989'],
+['All You Had To Do Was Stay','1989'],
+['Shake It Off','1989'],
+['I Wish You Would','1989'],
+['Bad Blood','1989'],
+['Wildest Dreams','1989'],
+['How You Get The Girl','1989'],
+['This Love','1989'],
+['I Know Places','1989'],
+['Clean','1989'],
+['Wonderland','1989'],
+['You Are In Love','1989'],
+['New Romantics','1989'],
+['...Ready For It?','Reputation'],
+['End Game','Reputation'],
+['I Did Something Bad','Reputation'],
+['Don\'t Blame Me','Reputation'],
+['Delivate','Reputation'],
+['Look What You Made Me Do','Reputation'],
+['So It Goes...','Reputation'],
+['Gorgeous','Reputation'],
+['Getaway Car','Reputation'],
+['King Of My Heart','Reputation'],
+['Dancing With Our Hands Tied','Reputation'],
+['Dress','Reputation'],
+['This Is Why We Can\'t Have Nice Things','Reputation'],
+['Call It What You Want','Reputation'],
+['New Year\'s Day','Reputation'],
+['I Forgot That You Existed','Lover'],
+['Cruel Summer','Lover'],
+['Lover','Lover'],
+['The Man','Lover'],
+['The Archer','Lover'],
+['I Think He Knows','Lover'],
+['KMiss AMericana & The Heartbreak Prince','Lover'],
+['Paper Rings','Lover'],
+['Cornelia Street','Lover'],
+['Death By A Thousand Cuts','Lover'],
+['London Boy','Lover'],
+['Soon You\'ll Get Better','Lover'],
+['False God','Lover'],
+['You Need To Calm Down','Lover'],
+['Afterglow','Lover'],
+['ME!','Lover'],
+['It\'s Nice To Have A Friend','Lover'],
+['Daylight','Lover'],
+['the 1','folklore'],
+['cardigan','folklore'],
+['the last great american dynasty','folklore'],
+['exile','folklore'],
+['my tears richochet','folklore'],
+['mirrorball','folklore'],
+['seven','folklore'],
+['august','folklore'],
+['this is me trying','folklore'],
+['illicit affairs','folklore'],
+['invisible string','folklore'],
+['mad woman','folklore'],
+['epiphany','folklore'],
+['betty','folklore'],
+['peace','folklore'],
+['hoax','folklore'],
+['the lakes','folklore'],
+['willow','evermore'],
+['champagne problems','evermore'],
+['gold rush','evermore'],
+['\'tis the damn season','evermore'],
+['tolerate it','evermore'],
+['no body, no crime','evermore'],
+['happiness','evermore'],
+['dorothea','evermore'],
+['coney island','evermore'],
+['ivy','evermore'],
+['cowboy like me','evermore'],
+['long story short','evermore'],
+['marjorie','evermore'],
+['closure','evermore'],
+['evermore','evermore'],
+['right where you left me','evermore'],
+['it\'s time to go','evermore'],
+['Jump Then Fall','Fearless (Taylor\'s Version)'],
+['Untouchable','Fearless (Taylor\'s Version)'],
+['Come In With The Rain','Fearless (Taylor\'s Version)'],
+['SuperStar','Fearless (Taylor\'s Version)'],
+['The Other Side Of the Door','Fearless (Taylor\'s Version)'],
+['Fearless','Fearless (Taylor\'s Version)'],
+['Fifteen','Fearless (Taylor\'s Version)'],
+['Love Story','Fearless (Taylor\'s Version)'],
+['Hey Stephen','Fearless (Taylor\'s Version)'],
+['White Horse','Fearless (Taylor\'s Version)'],
+['You Belong With Me','Fearless (Taylor\'s Version)'],
+['Breathe','Fearless (Taylor\'s Version)'],
+['Tell Me Why','Fearless (Taylor\'s Version)'],
+['You\'re Not Sorry','Fearless (Taylor\'s Version)'],
+['The Way I Loved You','Fearless (Taylor\'s Version)'],
+['Forever & Always','Fearless (Taylor\'s Version)'],
+['The Best Day','Fearless (Taylor\'s Version)'],
+['Change','Fearless (Taylor\'s Version)'],
+['Today Was A Fairytale','Fearless (Taylor\'s Version)'],
+['You All Over Me','Fearless (Taylor\'s Version)'],
+['Mr. Perfectly Fine','Fearless (Taylor\'s Version)'],
+['We Were Happy','Fearless (Taylor\'s Version)'],
+['That\'s When','Fearless (Taylor\'s Version)'],
+['Don\'t You','Fearless (Taylor\'s Version)'],
+['Bye Bye Baby','Fearless (Taylor\'s Version)'],
+['State of Grace','Red (Taylor\'s Version)'],
+['Red','Red (Taylor\'s Version)'],
+['Treacherous','Red (Taylor\'s Version)'],
+['I Knew You Were Trouble.','Red (Taylor\'s Version)'],
+['All Too Well','Red (Taylor\'s Version)'],
+['22','Red (Taylor\'s Version)'],
+['I Almost Do','Red (Taylor\'s Version)'],
+['We Are Never Ever Getting Back Together','Red (Taylor\'s Version)'],
+['Stay Stay Stay','Red (Taylor\'s Version)'],
+['The Last Time','Red (Taylor\'s Version)'],
+['Holy Ground','Red (Taylor\'s Version)'],
+['Sad Beautiful Tragic','Red (Taylor\'s Version)'],
+['The Lucky One','Red (Taylor\'s Version)'],
+['Everything Has Changed','Red (Taylor\'s Version)'],
+['Starlight','Red (Taylor\'s Version)'],
+['Begin Again','Red (Taylor\'s Version)'],
+['The Moment I knew','Red (Taylor\'s Version)'],
+['Come Back...Be Here','Red (Taylor\'s Version)'],
+['Girls At Home','Red (Taylor\'s Version)'],
+['Ronan','Red (Taylor\'s Version)'],
+['Better Man','Red (Taylor\'s Version)'],
+['Nothing New','Red (Taylor\'s Version)'],
+['Babe','Red (Taylor\'s Version)'],
+['Message In A Bottle','Red (Taylor\'s Version)'],
+['I Bet You Think About Me','Red (Taylor\'s Version)'],
+['Forever Winter','Red (Taylor\'s Version)'],
+['Run','Red (Taylor\'s Version)'],
+['The Very First Night','Red (Taylor\'s Version)'],
+['All Too Well (10 Minute Version)','Red (Taylor\'s Version)'],
+['Lavender Haze','Midnights'],
+['Maroon','Midnights'],
+['Anti-Hero','Midnights'],
+['Snow On The Beach','Midnights'],
+['You\'re On Your Own, Kid','Midnights'],
+['Midnight Rain','Midnights'],
+['Question...?','Midnights'],
+['Vigilante Shit','Midnights'],
+['Bejeweled','Midnights'],
+['Labryinth','Midnights'],
+['Karma','Midnights'],
+['Sweet Nothing','Midnights'],
+['Mastermind','Midnights'],
+['The Great War','Midnights'],
+['Bigger Than The Whole Sky','Midnights'],
+['Paris','Midnights'],
+['High Infidelity','Midnights'],
+['Glitch','Midnights'],
+['Would\'ve Could\'ve Should\'ve','Midnights'],
+['Dear Reader','Midnights']]
+
+var albums = ['Taylor Swift', 'Fearless', 'Speak Now', 'Red', '1989', 'Reputation', 'Lover', 'folklore', 'evermore', 'Fearless (Taylor\'s Version)', 'Red (Taylor\'s Version)', 'Midnights']
+
+var render = true
+
+var currentIndex = -1
+
+var seen = new Map()
+
+var scoreDict = new Map()
+var albumSize = new Map()
+
+let button1
+let button2
+
+function setup(){
+    var canvas = createCanvas(680, 460);
+    canvas.parent('sketch-holder');
+
+  	button1 = createButton('Never Skip');
+  	button1.parent('button-options');
+  	button1.position(-40, 20);
+  	button1.elt.style.position = 'relative'
+  	button1.mousePressed(function(){buttonPressed(1)});
+
+
+  	button2 = createButton('Sometimes Skip');
+  	button2.parent('button-options');
+  	button2.position(80, 20);
+  	button2.elt.style.position = 'relative'
+  	button2.mousePressed(function(){buttonPressed(0)});
+
+  	fill(255, 255, 255);
+}
+
+function buttonPressed(value) {
+	// console.log(currentIndex)
+	if (!scoreDict.get(songs[currentIndex][1])) {
+		scoreDict.set(songs[currentIndex][1], value)
+		albumSize.set(songs[currentIndex][1], 1)
+	} else {
+		scoreDict.set(songs[currentIndex][1], scoreDict.get(songs[currentIndex][1])+value)
+		albumSize.set(songs[currentIndex][1], albumSize.get(songs[currentIndex][1])+1)
+	}
+	render = true
+}
+
+function draw(){
+    if (render) {
+    	background(222, 165, 164)
+    	var currentIndexHolder = currentIndex
+    	if (seen.size === songs.length) {
+    		textAlign(LEFT)
+	    	textSize(24);
+    		button1.remove()
+    		button2.remove()
+    		for (let i = 0; i < albums.length; i++) { 
+  				var albumText = text(albums[i], 100, 30+(i*30));
+  				var score = Math.round((scoreDict.get(albums[i])/albumSize.get(albums[i]) * 100)) 
+  				console.log(score)
+  				var scoreText = text(str(score)+'%', 460, 30+(i*30));
+  				fill(255, 255, 255);
+			}
+    	} else {
+    		while(currentIndexHolder === currentIndex) {
+	    		var maybe = getRandomInt(0, songs.length-1)
+	    		if (seen.get(maybe) !== 1) {
+	    			currentIndex = maybe
+	    			seen.set(currentIndex, 1)
+	    		}
+    		}
+	    	textSize(32);
+    		textAlign(CENTER)
+			songText = text(songs[currentIndex][0], 340, 200);
+			albumText = text(songs[currentIndex][1], 340, 240);
+			fill(255, 255, 255);
+    	}
+    	render = false
+    }	
+    fill(255, 255, 255);
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
